658f94bad94f5a3ceefff1d12c27393592be1060
shikyonayuta <shikyonayuta@gmail.com>
2023-04-20 04:51:39 +0900

    UWU

---
+++ src/terminal/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {render, Box, Text} from 'ink';
+import { useTextInput } from 'ink-text-input';
+import { useSelectInput } from 'ink-select-input';
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <div className="app">
+    </div>
+  )
+}
+
+export default App
