2fd41d68c7d8096cefa5f3632edfa34958aeb3cf
shikyonayuta <shikyonayuta@gmail.com>
2023-04-20 04:54:46 +0900

    UWU

--- src/terminal/App.tsx
+++ src/terminal/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import {render, Box, Text} from 'ink';
-import { useTextInput } from 'ink-text-input';
-import { useSelectInput } from 'ink-select-input';
 
 function App() {
   const [] = useState(0)
